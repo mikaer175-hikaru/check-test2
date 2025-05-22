b0ba7a489ce0145f9421fd24728dd8bacfe61e00
hikaru_takei <mikaer175@example.com>
2025-05-22 22:28:35 +0900

    最終提出

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,10 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
-        $this->call(SeasonSeeder::class);
+        $this->call([
+            SeasonSeeder::class,
+            ProductSeeder::class,
+        ]);
 
     }
 }
